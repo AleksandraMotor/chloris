6fc402b7fd4bc8db7f3765500a5543c206a600bb
AleksandraMotor <aleksandramotor@gmail.com>
2022-10-05 00:57:39 +0200

    color-palette

---
+++ src/components/banner/banner.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import './banner.scss';
+
+const Banner = () => {
+
+    return (
+        <div className='container'>
+            <div className='container__paleta'>
+                <div className='container__paleta__color--1'>
+                </div>
+                <div className='container__paleta__color--2'>
+                </div>
+                <div className='container__paleta__color--3'>
+                </div>
+                <div className='container__paleta__color--4'>
+                </div>
+                <div className='container__paleta__color--5'>
+                </div>
+            </div>
+            <div className='container__paleta'>
+                <div className='container__paleta__color--1a'>
+                </div>
+                <div className='container__paleta__color--2a'>
+                </div>
+                <div className='container__paleta__color--3a'>
+                </div>
+                <div className='container__paleta__color--4a'>
+                </div>
+                <div className='container__paleta__color--5a'>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default Banner;
